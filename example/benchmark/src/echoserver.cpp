4afee282a8343a732a852a0009d19f62b18ead1b
shadow-paw <31270670+shadow-paw@users.noreply.github.com>
2019-04-21 05:22:46 +0000

    #42 fix unable to build example

--- example/benchmark/src/echoserver.cpp
+++ example/benchmark/src/echoserver.cpp
@@ -5,35 +5,28 @@ namespace example {
 EchoServer::EchoServer() : lightcone::NetEngine(&_lb) {
     // set_timeout(1000);
 }
-// -----------------------------------------------------------
 bool EchoServer::cb_net_timeout(lightcone::Tcp* conn, uint64_t now) {
-    return true;
+    return false;
 }
-// -----------------------------------------------------------
 bool EchoServer::cb_net_refused(lightcone::Tcp* conn, uint64_t now) {
     return true;
 }
-// -----------------------------------------------------------
 bool EchoServer::cb_net_accepted(lightcone::Tcp* conn, lightcone::Tcp* from, uint64_t now) {
     return true;
 }
-// -----------------------------------------------------------
 bool EchoServer::cb_net_opened(lightcone::Tcp* conn, uint64_t now) {
     return true;
 }
-// -----------------------------------------------------------
 bool EchoServer::cb_net_closed(lightcone::Tcp* conn, uint64_t now) {
     return true;
 }
-// -----------------------------------------------------------
 bool EchoServer::cb_net_recv(lightcone::Tcp* conn, uint64_t now) {
-    conn->recv([this, conn](const uint8_t* rbuf, size_t rlen) -> size_t {
+    conn->recv([conn](const uint8_t* rbuf, size_t rlen) -> size_t {
         conn->send(rbuf, rlen);
         return rlen;
     });
     return true;
 }
-// -----------------------------------------------------------
 bool EchoServer::cb_net_sent(lightcone::Tcp* conn, uint64_t now) {
     return true;
 }
